a76d5c3135e52776ed4a762be4e4ddbece1745c4
Alexander Hipp <al3xhipp@mail.com>
2019-04-16 17:48:42 +0200

    Added GridSearchCV

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -59,12 +59,21 @@ def build_model():
     :description: Builds machine learning pipeline
     :return: returns a model
     """
-    pipeline = Pipeline([('vect', CountVectorizer(tokenizer=tokenize, max_df=0.5)),
-                         ('tfidf', TfidfTransformer()),
-                         ('clf', MultiOutputClassifier(AdaBoostClassifier(DecisionTreeClassifier(max_depth=1,
-                                                                                                 criterion='gini'),
-                                                                          n_estimators=100)))])
-    return pipeline
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(random_state=1)))
+    ])
+    
+    parameters = {
+        'clf__estimator__criterion': ['gini', 'entropy'],
+        'clf__estimator__n_estimators': [5, 10, 20],
+        'clf__estimator__min_samples_split': [2, 4, 6]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=3, n_jobs=-2)
+    return cv
 
 
 def evaluate_model(model, X_test, y_test, category_names):
